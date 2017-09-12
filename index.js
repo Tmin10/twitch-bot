6063c2b9baa6173f2974df9f79155a26c99d47f4
Tmin10 <Tmin100@gmail.com>
2017-09-12 18:10:18 +0300

    Bot can write something to channel

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const TwitchBot = require('twitch-bot')
-const open = require('open')
-const readline = require('readline')
-const config = require('./config')
+const TwitchBot = require('twitch-bot');
+const open = require('open');
+const readline = require('readline');
+const config = require('./config');
+var request = require('request');
 
 var rl = readline.createInterface(process.stdin, process.stdout);
 
@@ -11,31 +12,50 @@ let authUrl = 'https://api.twitch.tv/kraken/oauth2/authorize' +
     '&response_type=code' +
     '&scope=chat_login';
 
-console.log('Oppening url: ' + authUrl);
+console.log('Opening url: ' + authUrl);
 open(authUrl);
 rl.question('Please copy and paste url code here: ', (code) => {
     processCode(code);
 });
 
 function processCode(code) {
-
+    let requestUrl = 'https://api.twitch.tv/kraken/oauth2/token' +
+        '?client_id=' + config.clientId +
+        '&client_secret=' + config.clientSecret +
+        '&code=' + code +
+        '&grant_type=authorization_code' +
+        '&redirect_uri=http://localhost';
+    request.post(
+        requestUrl,
+        {},
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                processToken(JSON.parse(body).access_token);
+            } else {
+                console.log(error, response);
+            }
+        }
+    );
 }
 
-const Bot = new TwitchBot({
-    username: 'Kappa_Bot',
-    oauth: 'oauth:dwiaj91j1KKona9j9d1420',
-    channel: 'twitch'
-})
+function processToken(token) {
+    const Bot = new TwitchBot({
+        username: config.userName,
+        oauth: 'oauth:' + token,
+        channel: config.channelName
+    });
 
-Bot.on('join', () => {
-    Bot.on('message', chatter => {
-        console.log(chatter)
-        if(chatter.message === '!test') {
-            Bot.say('Command executed! PogChamp')
-        }
+    Bot.on('join', () => {
+        Bot.say('Command executed! PogChamp');
+        Bot.on('message', chatter => {
+            console.log(chatter);
+            if(chatter.message === '!test') {
+                Bot.say('Command executed! PogChamp')
+            }
+        })
+    });
+
+    Bot.on('error', err => {
+        console.log(err)
     })
-})
- 
-Bot.on('error', err => {
-    console.log(err)
-})
+}
